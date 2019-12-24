6e0216723d0d83ae2063af741d7733716afe86cc
Sarika20 <nikitashete06@gmail.com>
2019-12-24 23:41:49 +0530

    asdfghjkl

---
+++ app/src/main/java/com/example/coesa/PresidentForm.java
@@ -0,0 +1,14 @@
+package com.example.coesa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PresidentForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_president_form);
+    }
+}
